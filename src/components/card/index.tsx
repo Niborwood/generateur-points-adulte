26e6c5cf5708d2ae9c8501fafe93fdaf37374f61
Niborwood <robin.souriau@gmail.com>
2022-04-15 19:40:30 +0200

    Added button for next question

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -8,12 +8,17 @@ import CardAnswers from "./card-answers";
 export default function Card({ question, goToNextQuestion }: CardProps) {
   return (
     <div className="relative px-4 py-8 bg-stone-100 rounded-xl drop-shadow-xl skew-y-1">
+      {/* Card Question */}
       <CardQuestion
         _id={question._id}
         title={question.title}
         category={question.category}
       />
+
+      {/* Card Answers */}
       <CardAnswers answers={question.answers} />
+
+      {/* Next Question Button */}
       <button
         className="flex justify-center w-full p-4 mt-16 mb-2 font-bold text-white transition-all ease-in-out rounded-xl drop-shadow-lg hover:drop-shadow-sm bg-fuchsia-600 hover:bg-green-600 shadow-blue-700"
         onClick={goToNextQuestion}
