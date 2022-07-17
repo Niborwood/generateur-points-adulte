ef95d9050965818671fce0aa8a5a9cf5cb843a38
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-07-17 19:45:25 +0200

    Better input style

--- src/components/card/card-name.tsx
+++ src/components/card/card-name.tsx
@@ -8,7 +8,7 @@ const CardName = ({ setQuizState }: CardNameProps) => {
       <h3 className="text-2xl font-bold">Quel est votre nom ?</h3>
       <input
         type="text"
-        className="w-full p-4 mt-8 mb-2 font-bold transition-all ease-in-out border-2 text-slate-900 rounded-xl drop-shadow-sm bg-purple-600/40 border-fuchsia-600"
+        className="w-full p-4 mt-8 mb-2 font-bold transition-all ease-in-out border-4 text-slate-100 rounded-xl drop-shadow-sm bg-emerald-600/80 border-emerald-700"
         onChange={(e) =>
           setQuizState((prev: QuizState) => ({
             ...prev,
