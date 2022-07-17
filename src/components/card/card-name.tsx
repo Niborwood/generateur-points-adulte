85de73272abc3daf6aaaa5e1a8858583c6e69548
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-07-17 19:56:32 +0200

    Added basic name check

--- src/components/card/card-name.tsx
+++ src/components/card/card-name.tsx
@@ -1,30 +1,31 @@
+import { useRef, useState } from "react";
 import Button from "../ui/button";
 import { QuizState } from "../../../definitions/definitions";
 import { Dispatch, SetStateAction } from "react";
 
 const CardName = ({ setQuizState }: CardNameProps) => {
+  const nameInput = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string>("");
+
+  // Check User Name
+  const checkUserName = () => {
+    const name = nameInput.current?.value;
+    if (name && name.trim().length > 0)
+      setQuizState((prev) => ({ ...prev, name }));
+    else setError("Merci d'entrer un nom.");
+  };
+
   return (
     <div>
       <h3 className="text-2xl font-bold">Quel est votre nom ?</h3>
       <input
+        ref={nameInput}
         type="text"
         className="w-full p-4 mt-8 mb-2 font-bold transition-all ease-in-out border-4 text-slate-100 rounded-xl drop-shadow-sm bg-emerald-600/80 border-emerald-700"
-        onChange={(e) =>
-          setQuizState((prev: QuizState) => ({
-            ...prev,
-            name: e.target.value,
-          }))
-        }
       />
-      <Button
-        text="Commencer"
-        onClick={() =>
-          setQuizState((prev) => ({
-            ...prev,
-            hasSetName: true,
-          }))
-        }
-      />
+      <div className="text-sm pl-2 text-red-500 mb-8">{error}</div>
+
+      <Button text="Commencer" onClick={checkUserName} />
     </div>
   );
 };
