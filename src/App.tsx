c5dd0f0e397bc523b9ce59ae8ead7d9c27a69d02
Robin Souriau <robin@diggersfactory.com>
2022-07-19 12:56:59 +0200

    Added routing basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from "react";
-
 // COMPONENT IMPORTS
-import Header from "./components/header/";
-import Quiz from "./components/quiz/quiz";
+import Header from "./components/header";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="flex-col min-h-screen font-headings bg-gradient-to-bl from-purple-700 via-fuchsia-700 to-pink-700">
       <Header />
       <div className="p-12">
-        <Quiz />
+        <Outlet />
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Quiz from "./components/quiz/quiz";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Quiz />} />
+          <Route path="/gpa-admin" element={<div>Hello</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
