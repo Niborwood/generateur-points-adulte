fed6bca5ca03745aca31cea3400b2a654cff7fbd
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-07-27 18:51:07 +0200

    First draft of dashboard UI

--- src/components/admin/dashboard.tsx
+++ src/components/admin/dashboard.tsx
@@ -1,23 +1,33 @@
+import { useEffect } from "react";
+
 // REDUX
-import { useAppSelector } from "../../hooks/redux";
+import { useAppSelector, useAppDispatch } from "../../hooks/redux";
+import { fetchQuestions } from "../../features/quiz/quizThunks";
 
 // COMPONENTS
 import { Button, Title } from "../../components/ui";
 
 const Dashboard = () => {
   const { questions } = useAppSelector((state) => state.quiz);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchQuestions());
+  }, []);
 
   return (
     <div className="">
       <Title title="Modifier les questions" />
-      <div className="space-y-4">
+      <div className="space-y-4 my-16">
         {questions.map((question, index) => (
-          <div className="flex justify-between rounded-lg border-dashed border-2 border-pink-200 px-4">
-            <div>{question.title_0}</div>
-            <div className="flex flex-row gap-2 items-center">
-              <div className="bg-pink-100 text-pink-500 flex items-center justify-center rounded-lg aspect-square h-4/5">
+          <div className="flex justify-between rounded-xl border-dashed border-2 border-pink-200 pr-4 items-center">
+            <div className="flex flex-row items-center">
+              <div className="bg-pink-100 text-pink-400 justify-center p-6 font-bold cursor-move">
                 {index + 1}
               </div>
+              <div className="font-bold text-xl px-4">{question.title_0}</div>
+            </div>
+            <div className="flex flex-row gap-2 items-center">
               <Button text="Modifier" />
             </div>
           </div>
--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,5 +1,9 @@
 const Title = ({ title }: TitleProps) => {
-  return <h3 className="text-2xl font-bold">{title}</h3>;
+  return (
+    <h3 className="text-3xl font-bold after:content-[''] after:w-full after:h-2 after:absolute relative after:bottom-0.5 after:bg-gradient-to-r after:from-pink-600/60 after:to-purple-800/40 inline-block after:left-1 after:-z-10">
+      {title}
+    </h3>
+  );
 };
 
 interface TitleProps {
