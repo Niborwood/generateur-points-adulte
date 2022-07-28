573851f8577c936e2fcf52e207582839345525ab
Robin Souriau <robin@diggersfactory.com>
2022-07-28 14:09:32 +0200

    Added login local with JWT

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,13 +1,22 @@
 // REDUX
-import { useAppSelector } from "../../hooks/redux";
+import { useAppSelector, useAppDispatch } from "../../hooks/redux";
+import { checkIfLoggedIn } from "../../features/user/userSlice";
 
 // COMPONENTS
 import { CardWrapper } from "../../components/ui";
 import Dashboard from "./dashboard";
 import Sign from "./sign";
+import { useEffect } from "react";
+import supabase from "../../../lib/supabase";
 
 const Admin = () => {
+  const dispatch = useAppDispatch();
   const { isAuthenticated } = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    const user = supabase.auth.user();
+    dispatch(checkIfLoggedIn(user));
+  }, []);
 
   return (
     <CardWrapper large={isAuthenticated}>
