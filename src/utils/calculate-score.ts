a01f3caf42593be68108c8fbbb023d0fd13706b2
Robin Souriau <robin@diggersfactory.com>
2022-08-04 14:00:38 +0200

    Reintroduced score

--- src/utils/calculate-score.ts
+++ src/utils/calculate-score.ts
@@ -1,29 +1,33 @@
 import { AnswerGiven, Question } from "../../definitions/definitions";
 
+const threeDecimals = (num: number) => {
+  return Math.round(num * 1000) / 1000;
+};
+
 export default ({ answers, questions }: CalculateScoreProps) => {
-  //   let adultScore = 0;
-  //   let respScore = 0;
+  let adultScore = 0;
+  let respScore = 0;
 
-  //   for (const givenAnswer of answers) {
-  //     const question = questions.find(
-  //       (question) => question._id === givenAnswer.questionId
-  //     );
+  for (const givenAnswer of answers) {
+    const question = questions.find(
+      (question) => question._id === givenAnswer.questionId
+    );
 
-  //     if (!question) continue;
-  //     const answer = question.answers.find(
-  //       (answer) => answer._id === givenAnswer.answerId
-  //     );
+    if (!question) continue;
+    const answer = question.answers.find(
+      (answer) => answer._id === givenAnswer.answerId
+    );
 
-  //     if (!answer) continue;
-  //     adultScore += answer.adultScore ? answer.adultScore : 0;
-  //     respScore += answer.respScore ? answer.respScore : 0;
-  //   }
-  //   const playerAdultScore = adultScore / questions.length / 10;
-  //   const playerRespScore = respScore / questions.length / 10;
+    if (!answer) continue;
+    adultScore += answer.adultScore ? answer.adultScore : 0;
+    respScore += answer.respScore ? answer.respScore : 0;
+  }
+  adultScore = adultScore / questions.length / 10;
+  respScore = respScore / questions.length / 10;
 
   return {
-    adultScore: 0.36,
-    respScore: 0.36,
+    adultScore: threeDecimals(adultScore),
+    respScore: threeDecimals(respScore),
   };
 };
 
