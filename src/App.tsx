2d1aa4ca945b7d29b4411434b93fab162e168cfe
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-08-05 18:08:19 +0200

    Better header positioning

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useAppSelector } from "./hooks/redux";
 function App() {
   const { hasSetName, hasEndedQuiz } = useAppSelector((state) => state.quiz);
   return (
-    <div className="flex-col min-h-screen font-regular bg-gradient-to-bl from-purple-700 via-fuchsia-700 to-pink-700">
+    <div className="flex-col min-h-screen overflow-x-hidden font-regular bg-gradient-to-bl from-purple-700 via-fuchsia-700 to-pink-700">
       {(!hasSetName || hasEndedQuiz) && <Header />}
       <div className="flex flex-col items-center p-2 md:p-12">
         <Outlet />
