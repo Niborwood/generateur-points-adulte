39c190d8a1b437d7f3b97f316beb3458ba349e94
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-08-05 22:16:29 +0200

    Changed title

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,8 +6,8 @@ export default function Header() {
       <div>
         <h2 className="text-4xl font-thin tracking-wider text-center uppercase skew-x-6 skew-y-3 sm:text-5xl md:text-7xl lg:text-8xl font-headings 2xl:text-9xl text-slate-100 drop-shadow-xl shadow-white">
           <Link to="/">
-            Generateur <br />
-            de points d'adultes
+            Le <br />
+            ieuv- o -metre
           </Link>
         </h2>
       </div>
