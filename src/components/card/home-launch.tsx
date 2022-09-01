0510dd9c5c5f0e7950db2a4391531434d1f968b5
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-09-01 23:35:56 +0200

    Added light button theme

--- src/components/card/home-launch.tsx
+++ src/components/card/home-launch.tsx
@@ -11,19 +11,15 @@ export default () => {
 
   return (
     <div className="flex flex-col items-center justify-center md:w-2/3">
-      <div className="items-center mb-12 space-y-8 text-3xl font-thin tracking-wider prose text-center md:text-5xl lg:mt-20 lg:text-6xl text-slate-100 font-headings">
+      <div className="items-center mb-12 space-y-4 text-3xl font-thin tracking-wider prose text-center md:space-y-8 md:text-5xl lg:mt-20 lg:text-6xl text-slate-100 font-headings">
         <p>
           Mesurez si vous etes <br /> adulte et responsable.
         </p>
-        <p className="text-xl tracking-wide md:text-2xl">
+        <p className="text-base tracking-wide md:text-xl">
           Peu importe votre age.
         </p>
       </div>
-      <Button
-        text="Lancer le quiz"
-        style="primary"
-        onClick={handleLaunchClick}
-      />
+      <Button text="Lancer le test" style="light" onClick={handleLaunchClick} />
     </div>
   );
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,6 +12,8 @@ const Button = ({
     switch (style) {
       case "secondary":
         return "bg-gradient-to-tr from-purple-600 to-purple-700 hover:to-purple-800 shadow-purple-600/30";
+      case "light":
+        return "bg-gray-100 hover:bg-gray-200 text-xl text-fuchsia-600 hover:text-fuchsia-700 shadow-slate-500/10 px-16";
       default:
         return "bg-gradient-to-tr from-pink-600 to-fuchsia-600 hover:to-fuchsia-700 shadow-fuchsia-600/30";
     }
@@ -20,7 +22,9 @@ const Button = ({
   return (
     <button
       type={type}
-      className={`flex justify-center ${full ? "w-full" : "w-fit-content"} ${
+      className={`flex justify-center mt-2 mb-2 font-bold text-white transition-all ease-in-out hover:shadow-md ${
+        full ? "w-full" : "w-fit-content"
+      } ${
         small
           ? "px-3 py-2 text-sm shadow-sm rounded-md"
           : "p-4 shadow-lg rounded-xl"
@@ -28,7 +32,7 @@ const Button = ({
         isLoading
           ? "from-slate-300 to-slate-500 hover:to-slate-500"
           : "from-purple-600 to-purple-900"
-      } ${getStyle()} mt-2 mb-2 font-bold text-white transition-all ease-in-out hover:shadow-md`}
+      } ${getStyle()}`}
       onClick={onClick}
     >
       {isLoading ? "Loading..." : text || children}
